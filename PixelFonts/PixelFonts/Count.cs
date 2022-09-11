947f83191f06243aaa5e394a22d62785cf735ae6
SlavaNik16 <70444635+SlavaNik16@users.noreply.github.com>
2022-09-11 21:55:06 +0300

    Add files via upload

---
+++ PixelFonts/PixelFonts/Count.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PixelFonts
+{
+     class Count
+    {
+        public int result;
+        public int i;
+        public int len;
+        public string str; 
+
+        public Count()
+        {
+            result = 0;
+            i = 0;
+            len = 0;
+        }
+       
+        public void Conv(ref int[] nums, int num)
+        {
+            do
+            {
+                result = num % 10;
+                nums[i] = result;
+                num /= 10;
+                i++;
+            }
+            while (num > 0);
+            i--;
+        }
+
+        public string Len(int Length, char sim)
+        {
+            len = Length * 2;
+            str = new string(sim, len);
+            return str;
+        }
+
+        public void Dva_Tri_Sem_start(ref int x,ref int y)
+        {
+            Console.SetCursorPosition(x, ++y);
+            Console.WriteLine(Len(1, '▀'));
+            Console.SetCursorPosition(x, --y);
+            Console.WriteLine(Len(3, '█'));
+            Console.SetCursorPosition(x += 4, ++y);
+            for (int j = 0; j < 3; j++, Console.SetCursorPosition(x, ++y))
+            {
+                Console.WriteLine(Len(1, '█'));
+            }
+            
+
+        }
+
+        public void Dva_Tri_Sem_end(ref int x, ref int y)
+        {
+            Console.SetCursorPosition(x -= 4, y);
+            Console.Write(Len(3, '█'));
+            Console.SetCursorPosition(x, --y);
+            Console.Write(Len(1, '▄'));
+
+        }
+    }
+}
---
+++ PixelFonts/PixelFonts/Program.cs
@@ -0,0 +1,221 @@
+﻿
+
+using System;
+using System.ComponentModel;
+using System.Runtime.Serialization.Formatters;
+
+namespace PixelFonts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число от 0 до 9: ");
+            var number = Console.ReadLine();
+            bool run = int.TryParse(number, out int num);
+            int[] nums = new int[10];
+            Count c = new Count();
+            int x = (Console.WindowWidth) / 8;
+            int y = (Console.WindowHeight) / 3;
+            Console.SetCursorPosition(x, y);
+            c.Conv(ref nums, num);
+            if (run)
+            {
+                while (c.i >= 0)
+                {
+                    Console.SetCursorPosition(x, y);
+                    switch (nums[c.i])
+                    {
+
+                        case 1:                         
+                            Console.Write(c.Len(1, '█'));
+                            Console.SetCursorPosition(x+=2, y++);
+                            for (int j = 0; j <= 6; j++,Console.SetCursorPosition(x, y++))
+                            {
+                                Console.WriteLine(c.Len(1, '█'));
+                            }
+                            Console.SetCursorPosition(x-=2, y-=2);
+                            Console.WriteLine(c.Len(3, '█'));
+                            c.i--;
+                            x+= 8;
+                            y = 10;
+                            break;
+
+                        case 2:
+                            c.Dva_Tri_Sem_start(ref x, ref y);
+                            Console.SetCursorPosition(x-=4,--y);
+                            Console.WriteLine(c.Len(2, '█'));
+                            Console.SetCursorPosition(x, ++y);
+                            for (int j = 0; j < 3; j++, Console.SetCursorPosition(x, ++y))
+                            {
+                                Console.Write(c.Len(1, '█'));
+                            }
+                            Console.SetCursorPosition(x, --y);
+                            Console.WriteLine(c.Len(3, '█'));
+
+                            x += 8;
+                            y = 10;
+                            c.i--;
+                            break;
+
+                        case 3:
+                            c.Dva_Tri_Sem_start(ref x, ref y);
+                            Console.SetCursorPosition(x-=2, --y);
+                            Console.WriteLine(c.Len(1, '█'));
+                            Console.SetCursorPosition(x+=2,++y);
+                            for (int j = 0; j < 2; j++, Console.SetCursorPosition(x, ++y))
+                            {
+                                Console.Write(c.Len(1, '█'));
+                            }
+                            Console.SetCursorPosition(x -= 4, --y);
+                            Console.WriteLine(c.Len(1, '▄'));
+                            Console.SetCursorPosition(x, ++y);
+                            Console.WriteLine(c.Len(3, '█'));
+                            x += 8;
+                            y = 10;
+                            c.i--;
+                            break;
+
+                        case 4:
+                            for(int j = 0; j < 3; j++, Console.SetCursorPosition(x, ++y))
+                            {
+                                Console.WriteLine(c.Len(1, '█'));
+                            }
+                            Console.WriteLine(c.Len(2, '█'));
+                            Console.SetCursorPosition(x+=4, y-=3);
+                            for (int j = 0; j < 7; j++, Console.SetCursorPosition(x, ++y))
+                            {
+                                Console.Write(c.Len(1, '█'));
+                            }
+
+                            x += 4;
+                            y = 10;
+                            c.i--;
+                            break;
+
+                        case 5:
+                            Console.Write(c.Len(2, '█'));
+                            for (int j = 0; j < 3; j++, Console.SetCursorPosition(x, ++y))
+                            {
+                                Console.WriteLine(c.Len(1, '█'));
+                            }
+                            Console.WriteLine(c.Len(2, '█'));
+                            Console.SetCursorPosition(x+=4, y);
+                            for (int j = 0; j < 3; j++, Console.SetCursorPosition(x, ++y))
+                            {
+                                Console.Write(c.Len(1, '█'));
+                            }
+                            c.Dva_Tri_Sem_end(ref x, ref y);
+
+                            x += 8;
+                            y = 10;
+                            c.i--;
+                            break;
+
+                        case 6:
+                            Console.Write(c.Len(3, '█'));
+                            Console.SetCursorPosition(x+=4, ++y);
+                            Console.Write(c.Len(1, '▀'));
+                            
+                            Console.SetCursorPosition(x -= 4, --y);
+                            for (int j = 0; j < 6; j++, Console.SetCursorPosition(x, ++y))
+                            {
+                                Console.Write(c.Len(1, '█'));
+                            }
+                            Console.Write(c.Len(2, '█'));
+                            Console.SetCursorPosition(x += 4, y);
+                            for (int j = 0; j < 3; j++, Console.SetCursorPosition(x, --y))
+                            {
+                                Console.Write(c.Len(1, '█'));
+                            }
+                            Console.SetCursorPosition(x-=2, y);
+                            Console.Write(c.Len(2, '█'));
+                            x += 6;
+                            y = 10;
+                            c.i--;
+                            break;
+
+                        case 7:
+                            c.Dva_Tri_Sem_start(ref x, ref y);
+                            
+                            for (int j = 0; j < 3; j++, Console.SetCursorPosition(x, ++y))
+                            {
+                                Console.Write(c.Len(1, '█'));
+                            }
+                     
+                            x += 4;
+                            y = 10;
+                            c.i--;
+                            break;
+
+                        case 8:
+
+                            for (int j = 0; j < 6; j++, Console.SetCursorPosition(x, ++y))
+                            {
+                                Console.Write(c.Len(1, '█'));
+                            }
+                            Console.Write(c.Len(3, '█'));
+                            Console.SetCursorPosition(x, y-=6);
+                            Console.Write(c.Len(2, '█'));
+                            Console.SetCursorPosition(x+=4, y);
+                            for (int j = 0; j < 6; j++, Console.SetCursorPosition(x, ++y))
+                            {
+                                Console.Write(c.Len(1, '█'));
+                            }
+                            Console.SetCursorPosition(x -=2, y-=3);
+                            Console.Write(c.Len(1, '█'));
+                            x += 6;
+                            y = 10;
+                            c.i--;
+                            break;
+                        case 9:
+
+                            for (int j = 0; j < 3; j++, Console.SetCursorPosition(x, ++y))
+                            {
+                                Console.Write(c.Len(1, '█'));
+                            }
+                            Console.Write(c.Len(2, '█'));
+                            Console.SetCursorPosition(x+=2, y -= 3);
+                            Console.Write(c.Len(1, '█'));
+                            Console.SetCursorPosition(x+=2, y);
+                            for (int j = 0; j < 6; j++, Console.SetCursorPosition(x, ++y))
+                            {
+                                Console.Write(c.Len(1, '█'));
+                            }
+
+                            c.Dva_Tri_Sem_end(ref x, ref y);
+                            x += 8;
+                            y = 10;
+                            c.i--;
+                            break;
+
+                        default:
+                            for(int j = 0; j < 6; j++, Console.SetCursorPosition(x, ++y))
+                            {
+                                Console.Write(c.Len(1, '█'));
+                            }
+                            Console.Write(c.Len(3, '█'));
+                            Console.SetCursorPosition(x, y -= 6);
+                            Console.Write(c.Len(2, '█'));
+                            Console.SetCursorPosition(x += 4, y);
+                            for (int j = 0; j < 6; j++, Console.SetCursorPosition(x, ++y))
+                            {
+                                Console.Write(c.Len(1, '█'));
+                            }
+                            x += 4;
+                            y = 10;
+                            c.i--;
+                            break;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка! Введенно не число!");
+            }
+            Console.SetCursorPosition(x, y);
+
+            Console.ReadKey();
+        }
+    }
+}
