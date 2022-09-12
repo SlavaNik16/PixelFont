fc0d9aee4486b016cc6d699534bd236469d50fd8
SlavaNik16 <70444635+SlavaNik16@users.noreply.github.com>
2022-09-12 21:40:15 +0300

    Add files via upload

---
+++ PixelFonts/PixelFonts/Count.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+namespace PixelFonts
+{
+     class Count
+    {
+        public int len;
+        public int i;
+        public string str; 
+        public Count()
+        {
+            i = 0;
+            len = 0;
+        }
+        public string Len(int Length, char sim)
+        {
+            len = Length * 2;
+            str = new string(sim, len);
+            return str;
+        }
+        public void Score(ref int[] nums, string number)
+        {
+            foreach (char all in number)
+            {
+                if (all == ' ')
+                {
+                    nums[i] = -1;
+                }
+                else if (all == '.')
+                {
+                    nums[i] = -2;
+                }
+                else if (all == ',')
+                {
+                    nums[i] = -3;
+                }
+                else
+                {
+                    try
+                    {
+                        nums[i] = int.Parse(char.ToString(all));
+                    }
+                    catch (FormatException)
+                    {
+                        Environment.Exit(0);
+                    }
+                }
+                i++;
+            }
+            i--;
+        }
+        public void drawfor(ref int x, ref int y)
+        {
+            for (int j = 0; j < 3; j++, Console.SetCursorPosition(x, ++y))
+            {
+                Console.Write(Len(1, '█'));
+            }
+        }
+        public void Dva_Tri_Sem_start(ref int x, ref int y)
+        {
+            Console.SetCursorPosition(x, ++y);
+            Console.WriteLine(Len(1, '▀'));
+            Console.SetCursorPosition(x, --y);
+            Console.WriteLine(Len(3, '█'));
+            Console.SetCursorPosition(x += 4, ++y);
+            for (int j = 0; j < 3; j++, Console.SetCursorPosition(x, ++y))
+            {
+                Console.WriteLine(Len(1, '█'));
+
+            }
+        }
+        public void Dva_Tri_Sem_end(ref int x, ref int y)
+        {
+            Console.SetCursorPosition(x -= 4, y);
+            Console.Write(Len(3, '█'));
+            Console.SetCursorPosition(x, --y);
+            Console.Write(Len(1, '▄'));
+
+        }
+    }
+}
