4c6314d94f4d0381d884fc708a32eed79ae37f3e
SlavaNik16 <70444635+SlavaNik16@users.noreply.github.com>
2022-09-14 17:51:09 +0300

    Add files via upload

---
+++ PixelFonts/PixelFonts/Count.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+namespace PixelFonts
+{
+     class Count
+    {
+        public int len;
+        public int i;
+        public string str;
+        public int shift;
+        public bool stroka = false;
+        public bool num = false;
+        public Count()
+        {
+            i = 0;
+            len = 0;
+            shift = 0;
+        }
+        public string Len(int Length, char sim)
+        {
+            len = Length * 2;
+            str = new string(sim, len);
+            return str;
+        }
+        public void Score(ref int[] nums, string number)
+        {
+            foreach (char all in number)
+            {
+                    if (all == ' ')
+                    {
+                        nums[i] = -1;
+                        shift += 2;
+                        stroka = true;
+                    }
+                    else if (all == '.')
+                    {
+                        nums[i] = -2;
+                        shift += 2;
+                        stroka = true;
+                    }
+                    else if (all == ',')
+                    {
+                        nums[i] = -3;
+                        shift += 2;
+                        stroka = true;
+                    }
+                    else
+                    {
+                        try
+                        {
+                            nums[i] = int.Parse(char.ToString(all));
+                            if (number.Length > 1)
+                            {
+                                shift += 4;
+                                num = true;
+                            } 
+                            
+                        }
+                        catch (FormatException)
+                        {
+                            Environment.Exit(0);
+                        }
+                    }
+               
+                i++;
+            }
+            i--;
+        }
+        public void drawfor(ref int x, ref int y)
+        {
+            for (int j = 0; j < 3; j++, Console.SetCursorPosition(x, ++y))
+            {
+                Console.Write(Len(1, '█'));
+            }
+        }
+        public void Dva_Tri_Sem_start(ref int x, ref int y)
+        {
+            Console.SetCursorPosition(x, ++y);
+            Console.WriteLine(Len(1, '▀'));
+            Console.SetCursorPosition(x, --y);
+            Console.WriteLine(Len(3, '█'));
+            Console.SetCursorPosition(x += 4, ++y);
+            for (int j = 0; j < 3; j++, Console.SetCursorPosition(x, ++y))
+            {
+                Console.WriteLine(Len(1, '█'));
+
+            }
+        }
+        public void Dva_Tri_Sem_end(ref int x, ref int y)
+        {
+            Console.SetCursorPosition(x -= 4, y);
+            Console.Write(Len(3, '█'));
+            Console.SetCursorPosition(x, --y);
+            Console.Write(Len(1, '▄'));
+
+        }
+    }
+}
---
+++ PixelFonts/PixelFonts/Program.cs
@@ -0,0 +1,212 @@
+﻿
+
+using System;
+
+
+namespace PixelFonts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.SetWindowSize(240, 60);
+            Console.Write("Введите число от 0 до 9 со знаками разделителями(Пробел, точкой, запятой): ");
+            var number = Console.ReadLine();
+            int[] nums = new int[20];
+            Count c = new Count();
+            c.Score(ref nums, number);
+            int x = (Console.WindowWidth) / 2;
+            int y = (Console.WindowHeight) / 3;
+            if (c.stroka && !c.num) c.shift -= 2;
+            if (c.num) c.shift -= 2;
+            
+            Console.SetCursorPosition(x-=c.shift, y);
+            int q = 0;
+            while (q <= c.i)
+            {
+                Console.SetCursorPosition(x, y);
+                switch (nums[q])
+                {
+                    case 1:
+                        Console.Write(c.Len(1, '█'));
+                        Console.SetCursorPosition(x += 2, y++);
+                        for (int j = 0; j <= 6; j++, Console.SetCursorPosition(x, y++))
+                        {
+                            Console.WriteLine(c.Len(1, '█'));
+                        }
+                        Console.SetCursorPosition(x -= 2, y -= 2);
+                        Console.WriteLine(c.Len(3, '█'));
+                        x += 8;
+                        y = (Console.WindowHeight) / 3;
+                        q++;
+                        break;
+
+                    case 2:
+                        c.Dva_Tri_Sem_start(ref x, ref y);
+                        Console.SetCursorPosition(x -= 4, --y);
+                        Console.WriteLine(c.Len(2, '█'));
+                        Console.SetCursorPosition(x, ++y);
+                        c.drawfor(ref x, ref y);
+                        Console.SetCursorPosition(x, --y);
+                        Console.WriteLine(c.Len(3, '█'));
+                        x += 8;
+                        y = (Console.WindowHeight) / 3;
+                        q++;
+                        break;
+
+                    case 3:
+                        c.Dva_Tri_Sem_start(ref x, ref y);
+                        Console.SetCursorPosition(x -= 2, --y);
+                        Console.WriteLine(c.Len(1, '█'));
+                        Console.SetCursorPosition(x += 2, ++y);
+                        for (int j = 0; j < 2; j++, Console.SetCursorPosition(x, ++y))
+                        {
+                            Console.Write(c.Len(1, '█'));
+                        }
+                        Console.SetCursorPosition(x -= 4, --y);
+                        Console.WriteLine(c.Len(1, '▄'));
+                        Console.SetCursorPosition(x, ++y);
+                        Console.WriteLine(c.Len(3, '█'));
+                        x += 8;
+                        y = (Console.WindowHeight) / 3;
+                        q++;
+                        break;
+
+                    case 4:
+                        c.drawfor(ref x, ref y);
+                        Console.WriteLine(c.Len(2, '█'));
+                        Console.SetCursorPosition(x += 4, y -= 3);
+                        for (int j = 0; j < 7; j++, Console.SetCursorPosition(x, ++y))
+                        {
+                            Console.Write(c.Len(1, '█'));
+                        }
+                        x += 4;
+                        y = (Console.WindowHeight) / 3;
+                        q++;
+                        break;
+
+                    case 5:
+                        Console.Write(c.Len(2, '█'));
+                        c.drawfor(ref x, ref y);
+                        Console.WriteLine(c.Len(2, '█'));
+                        Console.SetCursorPosition(x += 4, y);
+                        c.drawfor(ref x, ref y);
+                        c.Dva_Tri_Sem_end(ref x, ref y);
+                        x += 8;
+                        y = (Console.WindowHeight) / 3;
+                        q++;
+                        break;
+
+                    case 6:
+                        Console.Write(c.Len(3, '█'));
+                        Console.SetCursorPosition(x += 4, ++y);
+                        Console.Write(c.Len(1, '▀'));
+                        Console.SetCursorPosition(x -= 4, --y);
+                        for (int j = 0; j < 6; j++, Console.SetCursorPosition(x, ++y))
+                        {
+                            Console.Write(c.Len(1, '█'));
+                        }
+                        Console.Write(c.Len(2, '█'));
+                        Console.SetCursorPosition(x += 4, y);
+                        for (int j = 0; j < 3; j++, Console.SetCursorPosition(x, --y))
+                        {
+                            Console.Write(c.Len(1, '█'));
+                        }
+                        Console.SetCursorPosition(x -= 2, y);
+                        Console.Write(c.Len(2, '█'));
+                        x += 6;
+                        y = (Console.WindowHeight) / 3;
+                        q++;
+                        break;
+
+                    case 7:
+                        c.Dva_Tri_Sem_start(ref x, ref y);
+                        c.drawfor(ref x, ref y);
+                        x += 4;
+                        y = (Console.WindowHeight) / 3;
+                        q++;
+                        break;
+
+                    case 8:
+                        for (int j = 0; j < 6; j++, Console.SetCursorPosition(x, ++y))
+                        {
+                            Console.Write(c.Len(1, '█'));
+                        }
+                        Console.Write(c.Len(3, '█'));
+                        Console.SetCursorPosition(x, y -= 6);
+                        Console.Write(c.Len(2, '█'));
+                        Console.SetCursorPosition(x += 4, y);
+                        for (int j = 0; j < 6; j++, Console.SetCursorPosition(x, ++y))
+                        {
+                            Console.Write(c.Len(1, '█'));
+                        }
+                        Console.SetCursorPosition(x -= 2, y -= 3);
+                        Console.Write(c.Len(1, '█'));
+                        x += 6;
+                        y = (Console.WindowHeight) / 3;
+                        q++;
+                        break;
+                    case 9:
+                        c.drawfor(ref x, ref y);
+                        Console.Write(c.Len(2, '█'));
+                        Console.SetCursorPosition(x += 2, y -= 3);
+                        Console.Write(c.Len(1, '█'));
+                        Console.SetCursorPosition(x += 2, y);
+                        for (int j = 0; j < 6; j++, Console.SetCursorPosition(x, ++y))
+                        {
+                            Console.Write(c.Len(1, '█'));
+                        }
+                        c.Dva_Tri_Sem_end(ref x, ref y);
+                        x += 8;
+                        y = (Console.WindowHeight) / 3;
+                        q++;
+                        break;
+
+                    case -1:
+                        x += 4;
+                        y = (Console.WindowHeight) / 3;
+                        q++;
+                        break;
+
+                    case -2:
+                        Console.SetCursorPosition(x, y += 6);
+                        Console.Write(c.Len(1, '█'));
+                        x += 4;
+                        y = (Console.WindowHeight) / 3;
+                        q++;
+                        break;
+
+                    case -3:
+                        Console.SetCursorPosition(x, y+=6);
+                        Console.Write(c.Len(1, '█'));
+                        Console.SetCursorPosition(x, ++y);
+                        Console.Write(c.Len(1, '█'));
+                        x += 4;
+                        y = (Console.WindowHeight) / 3;
+                        q++;
+                        break;
+
+                    default:
+                        for (int j = 0; j < 6; j++, Console.SetCursorPosition(x, ++y))
+                        {
+                            Console.Write(c.Len(1, '█'));
+                        }
+                        Console.Write(c.Len(3, '█'));
+                        Console.SetCursorPosition(x, y -= 6);
+                        Console.Write(c.Len(2, '█'));
+                        Console.SetCursorPosition(x += 4, y);
+                        for (int j = 0; j < 6; j++, Console.SetCursorPosition(x, ++y))
+                        {
+                            Console.Write(c.Len(1, '█'));
+                        }
+                        x += 4;
+                        y = (Console.WindowHeight) / 3;
+                        q++;
+                        break;
+                }
+            }
+            Console.SetCursorPosition(x, y);
+            Console.ReadKey();
+        }
+    }
+}
